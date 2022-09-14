2f50c8bb1156d4f2e0ef37d21d2bfbf5ff802020
ejbaek <qordmswl5631@naver.com>
2022-09-14 18:16:38 +0900

    commit

--- assets/js/style.js
+++ assets/js/style.js
@@ -24,66 +24,6 @@ function progress() {
   }
 }
 
-window.addEventListener("load", function () {
-  gsap.registerPlugin(ScrollTrigger);
-
-  const pageContainer = document.querySelector("#main");
-  pageContainer.setAttribute("data-scroll-container", "");
-
-  const scroller = new LocomotiveScroll({
-    el: pageContainer,
-    smooth: true,
-    getDirection: true,
-  });
-
-  scroller.on("scroll", function (t) {
-    document.documentElement.setAttribute("data-direction", t.direction);
-  });
-
-  scroller.on("scroll", ScrollTrigger.update);
-
-  ScrollTrigger.scrollerProxy(pageContainer, {
-    scrollTop(value) {
-      return arguments.length
-        ? scroller.scrollTo(value, 0, 0)
-        : scroller.scroll.instance.scroll.y;
-    },
-    getBoundingClientRect() {
-      return {
-        left: 0,
-        top: 0,
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
-    },
-    pinType: pageContainer.style.transform ? "transform" : "fixed",
-  });
-
-  let horizontalSections = document.querySelectorAll(".horizontal-scroll");
-
-  horizontalSections.forEach((horizontalSection) => {
-    let pinWrap = horizontalSection.querySelector(".pin-wrap");
-    let pinWrapWidth = pinWrap.offsetWidth;
-    let horizontalScrollLength = pinWrapWidth - window.innerWidth;
-    gsap.to(pinWrap, {
-      scrollTrigger: {
-        scroller: "[data-scroll-container]",
-        scrub: true,
-        trigger: horizontalSection,
-        pin: true,
-        start: "top top",
-        end: () => `+=${pinWrapWidth}`,
-        invalidateOnRefresh: true,
-      },
-      x: -horizontalScrollLength,
-      ease: "none",
-    });
-  });
-
-  ScrollTrigger.addEventListener("refresh", () => scroller.update());
-  ScrollTrigger.refresh();
-});
-
 $(document).ready(function () {
   // 햄버거메뉴
   $(".btn-menu").click(function () {
@@ -191,3 +131,6 @@ function loading() {
   }, 10500);
 }
 loading();
+
+//Javascript
+//기본 세팅
